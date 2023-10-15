2bf148e877701bb4b366454399d3ada35659212e
Oskar Siry <oskarsiry99@gmail.com>
2023-10-15 11:24:46 +0200

    script for active link aded and srcolling animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//haburger menu
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav__menu');
 const icon = document.querySelector('.hamburger__icon');
@@ -8,3 +9,33 @@ hamburger.addEventListener('click', () => {
     ? (icon.src = 'assets/exit.png')
     : (icon.src = 'assets/hamburger.png');
 });
+
+// active links
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.active')?.classList.remove('active');
+    link.classList.add('active');
+  });
+});
+
+const sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top > offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+
+        document
+          .querySelector('nav a[href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
+};
